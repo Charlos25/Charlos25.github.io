82e1a39b38d27a94fa67cb3f65d2ebe0f634fcd2
Charlos25 <caralbasara007@gmail.com>
2024-09-09 12:11:09 -0500

    Dashboard

---
+++ DatosObservaciones.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+html_text = requests.get('https://cifras.biodiversidad.co/boyaca').text
+soup = BeautifulSoup(html_text, 'lxml')
+
+data_container = soup.find_all('div', class_ = 'bg-white flex flex-col justify-between text-black-2 py-3 px-4 w-auto gap-y-2 shadow-default hover:shadow-select')
+#print(product_list2)
+
+dato_pais = []
+dato_name = []
+# data_rows1 = []
+# data_rows2 = []
+data_valor1 = []
+data_valor2 = []
+
+# Iterar sobre los elementos y extraer la información
+for data in data_container:
+    pais = data.find('p', class_ = 'text-sm italic')
+    nombre = data.find('a')
+    
+    data_pais = pais.text.strip()
+    data_nombre = nombre.text.strip()
+    # Buscar las tablas dentro de cada contenedor
+    data_tables = data('table') # Esto devuelve una lista de tablas
+    
+    for table in data_tables:
+        #print(table.prettify()) # Imprimir cada tabla con formato legible
+        rows = table.find_all('th') # Buscar todas las filas de la tabla
+        if len(rows) > 1:
+            # data_row1 = rows[0].text.strip()
+            data_row2 = rows[1].text.strip()
+            # data_row3 = rows[2].text.strip()
+            data_row4 = rows[3].text.strip()
+            #print(data_row3)
+            # data_rows1.append(data_row1) 
+            data_valor1.append(data_row2.replace('.', '')) 
+            # data_rows2.append(data_row3)
+            data_valor2.append(data_row4.replace('.', ''))
+
+
+    
+    dato_pais.append(data_pais)   
+    dato_name.append(data_nombre)
+
+
+df=pd.DataFrame({
+    'País':dato_pais,
+    'Organización':dato_name,
+    'Observaciones':data_valor1,
+    'Especies':data_valor2,
+})
+
+print(df)
+
+df.to_csv('observacionEspecies.csv', index=False)
---
+++ EjercicioWeb.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template 
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def index():
+    df=pd.read_csv("observacionEspecies.csv")
+    data_dict=df.to_dict(orient="records")  # varialbel data_dicc asignar el csv a un diccionario
+    return render_template("dashboardBoy.html", data=data_dict)
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
